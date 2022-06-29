05583f657097d290a0e0c712ff532f1a9b4c366d
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-06-29 17:48:22 +0900

    fix: Modify by adding pagination function

--- src/hooks/useHomeDataFetch.js
+++ src/hooks/useHomeDataFetch.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import getStoryIds from "../utils/homeApi";
 
-const useHomeDataFetch = (type, value) => {
+const useHomeDataFetch = (type, start, last) => {
   // 스토리 데이터 저장
   const [stories, setStories] = useState([]);
   // 데이터 불러오기에 따른 로딩 상태
@@ -10,7 +10,7 @@ const useHomeDataFetch = (type, value) => {
   // 해당 hook이 호출 될때 실행
   useEffect(() => {
     setIsLoading(true);
-    getStoryIds(type, value)
+    getStoryIds(type, start, last)
       .then((stories) => {
         setStories(stories);
         setIsLoading(false);
@@ -18,7 +18,7 @@ const useHomeDataFetch = (type, value) => {
       .catch(() => {
         setIsLoading(false);
       });
-  }, [type]); // type 값이 변할때마다 실행된다.
+  }, [last]); // type 값이 변할때마다 실행된다.
 
   return { stories, isLoading };
 };
