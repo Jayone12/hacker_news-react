b669b3009037170b710b02581f93d481eb7f3445
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-07-01 00:16:49 +0900

    fix: Add url link

--- src/components/home/TodayTop.jsx
+++ src/components/home/TodayTop.jsx
@@ -1,10 +1,18 @@
 import { diffTime } from "../../utils/diffTime";
 
-const TodayTop = ({ index, value: { title, score, time, by } }) => {
+const TodayTop = ({ index, value: { title, score, time, by, url, id } }) => {
   return (
     <li>
       <div>{index}</div>
-      <div>{title}</div>
+      <div>
+        {url ? (
+          <a href={url} target="_blank" rel="noopener noreferrer">
+            {title}
+          </a>
+        ) : (
+          <a href={`/detail/${id}`}>{title}</a>
+        )}
+      </div>
       <div>
         <span>{by} </span>
         <span>{score} points </span>
