7b78b489243cc1ae44691d9820d203740b60f5a0
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-07-03 20:11:43 +0900

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
-import Home from "./routers/home/Home";
+import Home from "./pages/Home";
+import Top from "./pages/Top";
+import NotFound from "./pages/NotFound";
+import New from "./pages/New";
+import Show from "./pages/Show";
+import Ask from "./pages/Ask";
+import Job from "./pages/Job";
+import About from "./pages/About";
+import Detail from "./pages/Detail";
 
 function App() {
   return (
@@ -9,6 +17,14 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/top" element={<Top />} />
+        <Route path="/new" element={<New />} />
+        <Route path="/show" element={<Show />} />
+        <Route path="/ask" element={<Ask />} />
+        <Route path="/job" element={<Job />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/detail/:id" element={<Detail />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
