a8776c483a47e1a3609567ff29babb6aaa832a0b
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-07-03 22:06:29 +0900

    feat: Create dark theme styles

---
+++ src/utils/theme.js
@@ -0,0 +1,16 @@
+export const lightTheme = {
+  fontColor: "#202020",
+  bgColor: "#fff",
+  headerBg: "#ed702d",
+};
+
+export const darkTheme = {
+  fontColor: "#fff",
+  bgColor: "#292c33",
+  headerBg: "#292c33",
+};
+
+export const theme = {
+  lightTheme,
+  darkTheme,
+};
