afa11548731954f7ecc173bc1ceaf86e4909e14f
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-07-03 20:06:28 +0900

    feat: Create get Comment Api

--- src/utils/homeApi.js
+++ src/utils/homeApi.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const BASE_API_URL = "https://hacker-news.firebaseio.com/v0";
 
 // story ID를 토대로 데이터를 받아온다.
-const getStory = async (id) => {
+export const getStory = async (id) => {
   try {
     const { data } = await axios.get(`${BASE_API_URL}/item/${id}.json`);
     return data;
@@ -13,17 +13,29 @@ const getStory = async (id) => {
 };
 
 // 각 스토리의 ID를 type과 불러올 개수를 인자로 받는다.
-const getStoryIds = async (type, value) => {
+export const getStoryIds = async (type, start, last) => {
   try {
     const { data: storyIds } = await axios.get(
       `${BASE_API_URL}/${type}stories.json`
     );
     // 타입별 storyId를 갯수만큼 자르고 자른 수 만큼 story의 데이터를 불러온다.
-    const stories = await Promise.all(storyIds.slice(0, value).map(getStory));
+    const stories = await Promise.all(
+      storyIds.slice(start, last).map(getStory)
+    );
     return stories;
   } catch (error) {
     console.log(`storyId를 호출하는데 실패하였습니다.`, error);
   }
 };
 
-export default getStoryIds;
+export const getCommentIds = async (ids) => {
+  try {
+    if (ids === undefined) {
+      return;
+    }
+    const comments = await Promise.all(ids.map(getStory));
+    return comments;
+  } catch (error) {
+    console.log(`commentId를 호출하는데 실패하였습니다.`, error);
+  }
+};
